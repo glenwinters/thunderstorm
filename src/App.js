a4b8d6fed8e09f55e4d8b5fe2be60fb01813f84d
Glen Winters <lamouche@gmail.com>
2018-09-13 11:26:41 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <div className="warning-container">
+	  <p>The thunderstorms are coming!</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
