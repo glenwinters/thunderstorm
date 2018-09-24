9426580413b3a734ac071fc0a1cb1322deb4950e
Glen Winters <lamouche@gmail.com>
2018-09-24 10:50:43 -0500

    Add ThunderstormWarning component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import ThunderstormWarning from './ThunderstormWarning';
 
 class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <div className="warning-container">
-          <p>The thunderstorms are coming!</p>
-        </div>
+        <ThunderstormWarning />
       </div>
     );
   }
---
+++ src/ThunderstormWarning.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ThunderstormWarning = props => (
+  <div className="warning-container">
+    <p>The thunderstorms are coming!</p>
+  </div>
+);
+
+export default ThunderstormWarning;
